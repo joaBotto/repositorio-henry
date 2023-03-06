9e9cd805228d85bbc3d7c5610a05c44a8cce21dd
J7-B0TT0 <joaquinbotto2@hotmail.com>
2023-03-06 16:35:58 -0300

    General review

--- prueba1.js
+++ prueba1.js
@@ -1,5 +1,20 @@
-function dioxidoDeCloro(cds) {
-    console.log("hola" + cds)
+function juguetes(){
+let juguetesNinos = ['Pelota', 'Auto', 'Motito', 'Rasty']
+
+
+for (let i = 0; i < juguetesNinos.length; i++){
+    return juguetesNinos[i];
+
+}
 }
 
-dioxidoDeCloro + "Q tal?"
+
+function Auto(puertas, color, marca, año, ruedas){
+    this.puertas = puertas;
+    this.color = color;
+    this.marca = marca;
+    this.año = año;
+    this.ruedas = ruedas;
+}
+
+var miPrimerCarro = new Auto(4, 'Blue', 'Ford', 2023, 4)
